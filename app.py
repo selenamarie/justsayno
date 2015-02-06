0e98cfe7bc31a7556a383e76458c21868e799073
Selena Deckelmann <selenamarie@gmail.com>
2015-02-06 14:15:19 -0800

    Adding an api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from random import randint
 
 app = Flask(__name__)
@@ -21,6 +21,12 @@ def index():
     index = randint(0, len(WAYS_TO_SAY_NO) - 1)
     no = WAYS_TO_SAY_NO[index]
     return render_template('index.html', nope=no)
+
+@app.route('/api/nope')
+def api_nope():
+    index = randint(0, len(WAYS_TO_SAY_NO) - 1)
+    nope = {'say:': WAYS_TO_SAY_NO[index]}
+    return jsonify(nope)
 
 if __name__ == '__main__':
     app.run(debug=True)
